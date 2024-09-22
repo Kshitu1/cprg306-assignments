3614f2f7a1bd448e35add3fd34be87533e35d811
Kshitu1 <147000921+Kshitu1@users.noreply.github.com>
2024-09-21 20:57:56 -0600

    WEEK 2
    Week 2 Assignment Completed

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
-import Image from "next/image";
+import Link from 'next/link';
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <main>
-      
-    </main>
-    
+    <div>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <p>
+        Go to <Link href="/week-2">Week 2</Link> assignments.
+      </p>
+    </div>
   );
-  
-} 
+};
+
+export default HomePage;
---
+++ app/week-2/student-info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h2>Kshitiz Sapkota</h2>
+      <p>
+  <Link href="https://github.com/Kshitu1">
+    https://github.com
+  </Link>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
